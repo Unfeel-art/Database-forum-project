89fc4f5dbb8cca9e2a916b5e0ef81a9783aa0069
Unfeel-art <chernii.as12@gmail.com>
2025-10-25 01:48:56 +0200

    HW5 Full

--- hw5/public_html/js/theme.js
+++ hw5/public_html/js/theme.js
@@ -1,9 +1,6 @@
 const themeBtn = document.getElementById('theme-btn');
 const html = document.documentElement;
 
-const theme = localStorage.getItem('theme') || 'dark';
-html.setAttribute('data-theme', theme);
-
 themeBtn.addEventListener('click', () => {
     const curTheme = html.getAttribute('data-theme');
     const newTheme = curTheme === 'light' ? 'dark' : 'light';
